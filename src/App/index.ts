3c729458396f1e0e2a4246e31a2e5e326e7e82a4
basyair7 <basyair.fathul@gmail.com>
2024-06-08 16:58:04 +0700

    modified root project

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,39 +1,3 @@
-// import axios from "axios";
-// import * as dotenv from "dotenv";
-// import TelegramBot from "node-telegram-bot-api";
-
-// dotenv.config();
-
-// const telegramToken: string = process.env.TelebotTOKEN!;
-// const simSimiApiUrl: string = process.env.UrlSimSimi!;
-
-// const bot: TelegramBot = new TelegramBot(telegramToken, { polling: true });
-
-// bot.on("message", async (msg) => {
-//     const chatId: number = msg.chat.id;
-//     const message: string = msg.text?.toString()!;
-
-//     try {
-//         const response = await axios.post(simSimiApiUrl, {
-//             text: message,
-//             lc: "id"
-//         }, {
-//             headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded'
-//             }
-//         });
-
-//         const botReply: string = response.data.message;
-//         bot.sendMessage(chatId, botReply);
-//     }
-//     catch (error) {
-//         console.error(error);
-//         bot.sendMessage(chatId, `Error : ${error}`);
-//     }
-// });
-
-// console.log("Bot is running....");
-
 import axios from "axios"
 import TelegramBot from "node-telegram-bot-api"
 import keys from "../keys"
