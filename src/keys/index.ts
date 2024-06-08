989278d15a6b56f04cd09c71749c4be391cf7e82
basyair7 <basyair.fathul@gmail.com>
2024-06-08 18:02:38 +0700

    modified root project

--- src/keys/index.ts
+++ src/keys/index.ts
@@ -1,17 +1,5 @@
 import { Keys } from "../types"
 
-// const keys: Keys = {
-//     TeleBotToken: process.env.TELEBOT_TOKEN ?? 'nil',
-//     SimSimiAPIUrl: process.env.SIMSIMI_APIURL ?? 'nil',
-//     SimSimiApiKeys: process.env.SIMSIMI_APIKEYS ?? 'nil',
-// };
-
-// if (Object.values(keys).includes('nil')){
-//     throw new Error("Not all ENV variables are defined!");
-// }
-
-// export default keys;
-
 class KeyManager implements Keys {
     public TeleBotToken: string;
     public SimSimiAPIUrl: string;
